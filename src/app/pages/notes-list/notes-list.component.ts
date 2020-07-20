7f1838a67e9224ab526b2e67718d987201045eaf
T <gauquelintimothe@gmail.com>
2020-07-20 23:33:14 +0200

    Update Notes List
    Add the ngFor Note Card & Data Binding within

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Note } from 'src/app/shared/note.model';
+import { NotesService } from 'src/app/shared/notes.service';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+
 })
+
 export class NotesListComponent implements OnInit {
 
-  essai: string = 'abc';
+ notes: Note[] = new Array<Note>(); 
 
-  constructor() { }
+  constructor(private notesService: NotesService) { }
 
   ngOnInit() {
+    // We want to retrieve all notes from NotesService
+    this.notes = this.notesService.getAll();
   }
 
+  deleteNote(id: number) {
+    this.notesService.delete(id);
+  }
 }
