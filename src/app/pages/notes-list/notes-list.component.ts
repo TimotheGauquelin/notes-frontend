c4bc8758bd7d9d7db250570d5de9fce9ad07ab97
T <gauquelintimothe@gmail.com>
2020-07-29 23:11:13 +0200

    Filter Function

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -12,15 +12,62 @@ import { NotesService } from 'src/app/shared/notes.service';
 export class NotesListComponent implements OnInit {
 
  notes: Note[] = new Array<Note>(); 
+ filteredNotes: Note[] = new Array<Note>();
 
   constructor(private notesService: NotesService) { }
 
   ngOnInit() {
     // We want to retrieve all notes from NotesService
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number) {
     this.notesService.delete(id);
+  }
+
+  filter(query: string) {
+    query = query.toLowerCase().trim();
+
+    let allResults: Note[] = new Array<Note>();
+    //split up the search query into individual words
+    let terms: string[] = query.split(' '); //split on spaces
+    // remove duplicate search terms
+    terms = this.removeDuplicates(terms);
+    //complie all relevant results into the allResults array
+    terms.forEach(term => {
+      let results: Note[] = this.relevantNotes(term);
+      //append results to the allResults array
+      allResults = [...allResults, ...results]
+    });
+
+    // allResults will include duplicate notes
+    // because a particular note can be the result of many search terms
+    // bu we don't want to show the same note multiple times on the UI
+    // so we first must remove the duplicates
+    let uniqueResults = this.removeDuplicates(allResults);
+    this.filteredNotes = uniqueResults;
+  }
+
+  removeDuplicates(arr: Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+    //loop through the input array and add the items to the set
+    arr.forEach(e => uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  }
+
+  relevantNotes(query: any) : Array<Note> {
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if (note.body && note.body.toLocaleLowerCase().includes(query)) {
+        return true;
+      }
+      if (note.title && note.title.toLocaleLowerCase().includes(query)) {
+        return true;
+      }
+      return false;
+    })
+    return relevantNotes;
   }
 }
